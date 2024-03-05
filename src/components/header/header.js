db84d15d1c3fbe2deb62b091ef444fda4bcf274c
ArteDela <artedelafiamma@mail.ru>
2024-03-05 17:51:03 +0300

    Правки от заказчика

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -1,11 +1,19 @@
 mainObject.header =
 {
+	list: document.querySelector('._header__list'),
+	body: document.querySelector('body'),
+	burger: document.querySelector('._burger'),
 	show(instance)
 	{
 		let list = instance.closest('header').querySelector('._header__list')
-		let body = document.querySelector('body')
 		instance.classList.toggle('active')	
-		body.classList.toggle('active')	
+		this.body.classList.toggle('active')	
 		list.classList.toggle('active')	
+	},
+	closeEl()
+	{
+		this.body.classList.remove('active')	
+		this.list.classList.remove('active')	
+		this.burger.classList.remove('active')	
 	}
 }
